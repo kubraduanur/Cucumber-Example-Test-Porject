b0117cfd0a406e5992462e7bd86bd00ca33f30b3
kubraduanur <38734630+kubraduanur@users.noreply.github.com>
2023-11-24 17:11:59 +0300

    Update ReturnedPage.java

--- src/test/java/support/page_object/ReturnedPage.java
+++ src/test/java/support/page_object/ReturnedPage.java
@@ -7,6 +7,7 @@ import support.utils.BaseUtils;
 
 import static com.trendyol.mpapi.finance.helper.GrocerySettlementHelper.*;
 import static com.trendyol.mpapi.finance.matcher.DeliveredMatcher.isTrueSettlementDelivered;
+import static com.trendyol.mpapi.finance.helper.GrocerySettlementHelper.createGroceryOrderWithDiscount;
 import static com.trendyol.mpapi.finance.matcher.ReturnedMatcher.isTrueSettlementReturned;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -16,7 +17,7 @@ public class ReturnedPage {
 
     @Test
     public void shouldReturnedOrderWithDiscount() {
-        orderSummary = createGroceryOrderWithoutDiscount(base.createSellers(), true,2,false);
+        orderSummary = createGroceryOrderWithDiscount(base.createSellers(), true,2,false);
         changeGroceryStatus(orderSummary,"Delivered");
         updateLineItemsForClaim(orderSummary);
         assertThat("Settlement should be correct. Order number: " + orderSummary.getOrderNumber(), isTrueSettlementReturned(orderSummary));
