a318408c0f0b7c50070b25cff90244ae9651d695
kubraduanur <38734630+kubraduanur@users.noreply.github.com>
2023-11-24 17:10:51 +0300

    Update CancelledPage.java

--- src/test/java/support/page_object/CancelledPage.java
+++ src/test/java/support/page_object/CancelledPage.java
@@ -4,7 +4,7 @@ import mpapi.models.orderSummary.OrderSummary;
 import org.junit.jupiter.api.Test;
 import support.utils.BaseUtils;
 
-import static com.trendyol.mpapi.finance.helper.GrocerySettlementHelper.createGroceryOrderWithoutDiscount;
+import static com.trendyol.mpapi.finance.helper.GrocerySettlementHelper.createGroceryOrderWithDiscount;
 import static com.trendyol.mpapi.finance.helper.GrocerySettlementHelper.updateLineItemsForCancelWithReasonCode;
 import static com.trendyol.mpapi.finance.matcher.CancelledMatcher.isTrueSettlementCanceled;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,7 +15,7 @@ public class CancelledPage {
 
     @Test
     public void shouldCancelledOrderWithDiscount() {
-        orderSummary = createGroceryOrderWithoutDiscount(base.createSellers(), false,3,false);
+        orderSummary = createGroceryOrderWithDiscount(base.createSellers(), false,3,false);
         updateLineItemsForCancelWithReasonCode(orderSummary);
         assertThat("Settlement should be correct. Order number: " + orderSummary.getOrderNumber(), isTrueSettlementCanceled(orderSummary));
 
